f3285e477712a9e08ef6f397ae4d4e35120ff1e9
varianus <marcocas@gmail.com>
2015-02-09 15:29:48 +0100

    Remove usesless dependencies

--- umain.pas
+++ umain.pas
@@ -5,9 +5,9 @@ unit umain;
 interface
 
 uses
-  Classes, SysUtils, XMLConf, FileUtil, LResources, Forms, Controls, Graphics,
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics,
   Dialogs, ActnList, Menus, ComCtrls, StdActns, uEditor, LCLType,
-  XMLPropStorage, SynEditTypes, SynHighlighterPas, mrumanager, Config;
+  SynEditTypes, SynHighlighterPas, mrumanager, Config;
 
 type
 
