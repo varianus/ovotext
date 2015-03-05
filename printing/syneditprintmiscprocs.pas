b4d7996ea5c8bf7debcc52c50fe7425478328a6f
varianus <marcocas@gmail.com>
2015-03-05 11:40:41 +0100

    Converted to Lazarus printing codes of SynEdit

---
+++ printing/syneditprintmiscprocs.pas
@@ -0,0 +1,377 @@
+unit SynEditPrintMiscProcs;
+
+interface
+
+uses
+  LCLIntf, LCLType, Classes, SynEditTypes, Graphics;
+
+// Converting tabs to spaces: To use the function several times it's better
+// to use a function pointer that is set to the fastest conversion function.
+type
+  TConvertTabsProc = function(const Line: UnicodeString;
+    TabWidth: Integer): UnicodeString;
+
+function GetBestConvertTabsProc(TabWidth: Integer): TConvertTabsProc;
+// This is the slowest conversion function which can handle TabWidth <> 2^n.
+function ConvertTabs(const Line: UnicodeString; TabWidth: Integer): UnicodeString;
+
+type
+  TConvertTabsProcEx = function(const Line: UnicodeString; TabWidth: Integer;
+    var HasTabs: Boolean): UnicodeString;
+
+function GetBestConvertTabsProcEx(TabWidth: Integer): TConvertTabsProcEx;
+// This is the slowest conversion function which can handle TabWidth <> 2^n.
+function ConvertTabsEx(const Line: UnicodeString; TabWidth: Integer;
+  var HasTabs: Boolean): UnicodeString;
+
+function GetExpandedLength(const aStr: UnicodeString; aTabWidth: Integer): Integer;
+
+function CharIndex2CaretPos(Index, TabWidth: Integer;
+  const Line: UnicodeString): Integer;
+function CaretPos2CharIndex(Position, TabWidth: Integer; const Line: UnicodeString;
+  var InsideTabChar: Boolean): Integer;
+implementation
+
+// Please don't change this function; no stack frame and efficient register use.
+function GetHasTabs(pLine: PWideChar; var CharsBefore: Integer): Boolean;
+begin
+  CharsBefore := 0;
+  if Assigned(pLine) then
+  begin
+    while pLine^ <> #0 do
+    begin
+      if pLine^ = #9 then break;
+      Inc(CharsBefore);
+      Inc(pLine);
+    end;
+    Result := pLine^ = #9;
+  end
+  else
+    Result := False;
+end;
+
+
+function ConvertTabs1Ex(const Line: UnicodeString; TabWidth: Integer;
+  var HasTabs: Boolean): UnicodeString;
+var
+  pDest: PWideChar;
+  nBeforeTab: Integer;
+begin
+  Result := Line;  // increment reference count only
+  if GetHasTabs(pointer(Line), nBeforeTab) then
+  begin
+    HasTabs := True;
+    pDest := @Result[nBeforeTab + 1]; // this will make a copy of Line
+    // We have at least one tab in the string, and the tab width is 1.
+    // pDest points to the first tab char. We overwrite all tabs with spaces.
+    repeat
+      if (pDest^ = #9) then pDest^ := ' ';
+      Inc(pDest);
+    until (pDest^ = #0);
+  end
+  else
+    HasTabs := False;
+end;
+
+function ConvertTabs1(const Line: UnicodeString; TabWidth: Integer): UnicodeString;
+var
+  HasTabs: Boolean;
+begin
+  Result := ConvertTabs1Ex(Line, TabWidth, HasTabs);
+end;
+
+function ConvertTabs2nEx(const Line: UnicodeString; TabWidth: Integer;
+  var HasTabs: Boolean): UnicodeString;
+var
+  i, DestLen, TabCount, TabMask: Integer;
+  pSrc, pDest: PWideChar;
+begin
+  Result := Line;  // increment reference count only
+  if GetHasTabs(pointer(Line), DestLen) then
+  begin
+    HasTabs := True;
+    pSrc := @Line[1 + DestLen];
+    // We have at least one tab in the string, and the tab width equals 2^n.
+    // pSrc points to the first tab char in Line. We get the number of tabs
+    // and the length of the expanded string now.
+    TabCount := 0;
+    TabMask := (TabWidth - 1) xor $7FFFFFFF;
+    repeat
+      if pSrc^ = #9 then
+      begin
+        DestLen := (DestLen + TabWidth) and TabMask;
+        Inc(TabCount);
+      end
+      else
+        Inc(DestLen);
+      Inc(pSrc);
+    until (pSrc^ = #0);
+    // Set the length of the expanded string.
+    SetLength(Result, DestLen);
+    DestLen := 0;
+    pSrc := PWideChar(Line);
+    pDest := PWideChar(Result);
+    // We use another TabMask here to get the difference to 2^n.
+    TabMask := TabWidth - 1;
+    repeat
+      if pSrc^ = #9 then
+      begin
+        i := TabWidth - (DestLen and TabMask);
+        Inc(DestLen, i);
+        //This is used for both drawing and other stuff and is meant to be #9 and not #32
+        repeat
+          pDest^ := #9;
+          Inc(pDest);
+          Dec(i);
+        until (i = 0);
+        Dec(TabCount);
+        if TabCount = 0 then
+        begin
+          repeat
+            Inc(pSrc);
+            pDest^ := pSrc^;
+            Inc(pDest);
+          until (pSrc^ = #0);
+          exit;
+        end;
+      end
+      else
+      begin
+        pDest^ := pSrc^;
+        Inc(pDest);
+        Inc(DestLen);
+      end;
+      Inc(pSrc);
+    until (pSrc^ = #0);
+  end
+  else
+    HasTabs := False;
+end;
+
+function ConvertTabs2n(const Line: UnicodeString; TabWidth: Integer): UnicodeString;
+var
+  HasTabs: Boolean;
+begin
+  Result := ConvertTabs2nEx(Line, TabWidth, HasTabs);
+end;
+
+function ConvertTabsEx(const Line: UnicodeString; TabWidth: Integer;
+  var HasTabs: Boolean): UnicodeString;
+var
+  i, DestLen, TabCount: Integer;
+  pSrc, pDest: PWideChar;
+begin
+  Result := Line;  // increment reference count only
+  if GetHasTabs(pointer(Line), DestLen) then
+  begin
+    HasTabs := True;
+    pSrc := @Line[1 + DestLen];
+    // We have at least one tab in the string, and the tab width is greater
+    // than 1. pSrc points to the first tab char in Line. We get the number
+    // of tabs and the length of the expanded string now.
+    TabCount := 0;
+    repeat
+      if pSrc^ = #9 then
+      begin
+        DestLen := DestLen + TabWidth - DestLen mod TabWidth;
+        Inc(TabCount);
+      end
+      else
+        Inc(DestLen);
+      Inc(pSrc);
+    until (pSrc^ = #0);
+    // Set the length of the expanded string.
+    SetLength(Result, DestLen);
+    DestLen := 0;
+    pSrc := PWideChar(Line);
+    pDest := PWideChar(Result);
+    repeat
+      if pSrc^ = #9 then
+      begin
+        i := TabWidth - (DestLen mod TabWidth);
+        Inc(DestLen, i);
+        repeat
+          pDest^ := #9;
+          Inc(pDest);
+          Dec(i);
+        until (i = 0);
+        Dec(TabCount);
+        if TabCount = 0 then
+        begin
+          repeat
+            Inc(pSrc);
+            pDest^ := pSrc^;
+            Inc(pDest);
+          until (pSrc^ = #0);
+          exit;
+        end;
+      end
+      else
+      begin
+        pDest^ := pSrc^;
+        Inc(pDest);
+        Inc(DestLen);
+      end;
+      Inc(pSrc);
+    until (pSrc^ = #0);
+  end
+  else
+    HasTabs := False;
+end;
+
+function ConvertTabs(const Line: UnicodeString; TabWidth: Integer): UnicodeString;
+var
+  HasTabs: Boolean;
+begin
+  Result := ConvertTabsEx(Line, TabWidth, HasTabs);
+end;
+
+function IsPowerOfTwo(TabWidth: Integer): Boolean;
+var
+  nW: Integer;
+begin
+  nW := 2;
+  repeat
+    if (nW >= TabWidth) then break;
+    Inc(nW, nW);
+  until (nW >= $10000);  // we don't want 64 kByte spaces...
+  Result := (nW = TabWidth);
+end;
+
+function GetBestConvertTabsProc(TabWidth: Integer): TConvertTabsProc;
+begin
+  if (TabWidth < 2) then Result := TConvertTabsProc(@ConvertTabs1)
+    else if IsPowerOfTwo(TabWidth) then
+      Result := TConvertTabsProc(@ConvertTabs2n)
+    else
+      Result := TConvertTabsProc(@ConvertTabs);
+end;
+
+function GetBestConvertTabsProcEx(TabWidth: Integer): TConvertTabsProcEx;
+begin
+  if (TabWidth < 2) then Result := @ConvertTabs1Ex
+    else if IsPowerOfTwo(TabWidth) then
+      Result := @ConvertTabs2nEx
+    else
+      Result := @ConvertTabsEx;
+end;
+
+function GetExpandedLength(const aStr: UnicodeString; aTabWidth: Integer): Integer;
+var
+  iRun: PWideChar;
+begin
+  Result := 0;
+  iRun := PWideChar(aStr);
+  while iRun^ <> #0 do
+  begin
+    if iRun^ = #9 then
+      Inc(Result, aTabWidth - (Result mod aTabWidth))
+    else
+      Inc(Result);
+    Inc(iRun);
+  end;
+end;
+
+function CharIndex2CaretPos(Index, TabWidth: Integer;
+  const Line: UnicodeString): Integer;
+var
+  iChar: Integer;
+  pNext: PWideChar;
+begin
+// possible sanity check here: Index := Max(Index, Length(Line));
+  if Index > 1 then
+  begin
+    if (TabWidth <= 1) or not GetHasTabs(pointer(Line), iChar) then
+      Result := Index
+    else
+    begin
+      if iChar + 1 >= Index then
+        Result := Index
+      else
+      begin
+        // iChar is number of chars before first #9
+        Result := iChar;
+        // Index is *not* zero-based
+        Inc(iChar);
+        Dec(Index, iChar);
+        pNext := @Line[iChar];
+        while Index > 0 do
+        begin
+          case pNext^ of
+            #0:
+              begin
+                Inc(Result, Index);
+                break;
+              end;
+            #9:
+              begin
+                // Result is still zero-based
+                Inc(Result, TabWidth);
+                Dec(Result, Result mod TabWidth);
+              end;
+            else
+              Inc(Result);
+          end;
+          Dec(Index);
+          Inc(pNext);
+        end;
+        // done with zero-based computation
+        Inc(Result);
+      end;
+    end;
+  end
+  else
+    Result := 1;
+end;
+
+function CaretPos2CharIndex(Position, TabWidth: Integer; const Line: UnicodeString;
+  var InsideTabChar: Boolean): Integer;
+var
+  iPos: Integer;
+  pNext: PWideChar;
+begin
+  InsideTabChar := False;
+  if Position > 1 then
+  begin
+    if (TabWidth <= 1) or not GetHasTabs(pointer(Line), iPos) then
+      Result := Position
+    else
+    begin
+      if iPos + 1 >= Position then
+        Result := Position
+      else
+      begin
+        // iPos is number of chars before first #9
+        Result := iPos + 1;
+        pNext := @Line[Result];
+        // for easier computation go zero-based (mod-operation)
+        Dec(Position);
+        while iPos < Position do
+        begin
+          case pNext^ of
+            #0: break;
+            #9: begin
+                  Inc(iPos, TabWidth);
+                  Dec(iPos, iPos mod TabWidth);
+                  if iPos > Position then
+                  begin
+                    InsideTabChar := True;
+                    break;
+                  end;
+                end;
+            else
+              Inc(iPos);
+          end;
+          Inc(Result);
+          Inc(pNext);
+        end;
+      end;
+    end;
+  end
+  else
+    Result := Position;
+end;
+
+
+end.
+
