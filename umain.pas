128e5d23b5ea6548cd1355000e4c8f875528955d
varianus@github.com <marcocas@gmail.com>
2018-06-18 15:06:07 +0200

    Rename status bar

--- umain.pas
+++ umain.pas
@@ -160,7 +160,7 @@ type
     SearchFindNext1: TAction;
     SearchReplace: TAction;
     lbMessage: TStaticText;
-    StatusBar1: TStatusBar;
+    StatusBar: TStatusBar;
     ToolBar1: TToolBar;
     ToolButton1: TToolButton;
     ToolButton10: TToolButton;
@@ -211,7 +211,6 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure FormDropFiles(Sender: TObject; const FileNames: array of string);
-    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure HelpAboutExecute(Sender: TObject);
     procedure actLowerCaseExecute(Sender: TObject);
     procedure MenuItem28Click(Sender: TObject);
@@ -314,7 +313,7 @@ end;
 
 procedure TfMain.AppPropertiesShowHint(var HintStr: string; var CanShow: boolean; var HintInfo: THintInfo);
 begin
-  StatusBar1.Panels[3].Text := HintInfo.HintStr;
+  StatusBar.Panels[3].Text := HintInfo.HintStr;
 end;
 
 procedure TfMain.ActionListUpdate(AAction: TBasicAction; var Handled: boolean);
@@ -680,12 +679,6 @@ begin
 
 end;
 
-procedure TfMain.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState
-  );
-begin
-
-end;
-
 procedure TfMain.HelpAboutExecute(Sender: TObject);
 begin
   with TfAbout.Create(self) do
@@ -794,17 +787,17 @@ begin
   Editor := EditorFactory.CurrentEditor;
 
   if (scCaretX in Changes) or (scCaretY in Changes) then
-    StatusBar1.Panels[0].Text := Format(RSStatusBarPos, [Editor.CaretY, Editor.CaretX]);
+    StatusBar.Panels[0].Text := Format(RSStatusBarPos, [Editor.CaretY, Editor.CaretX]);
 
   if (scSelection in Changes) then
-    StatusBar1.Panels[1].Text :=
+    StatusBar.Panels[1].Text :=
       Format(RSStatusBarSel, [Editor.SelEnd - Editor.SelStart]);
 
   if (scInsertMode in Changes) then
     if Editor.InsertMode then
-      StatusBar1.Panels[2].Text := RSStatusBarInsMode
+      StatusBar.Panels[2].Text := RSStatusBarInsMode
     else
-      StatusBar1.Panels[2].Text := RSStatusBarOvrMode;
+      StatusBar.Panels[2].Text := RSStatusBarOvrMode;
 
   if (scModified in Changes) then
     if Editor.Modified then
